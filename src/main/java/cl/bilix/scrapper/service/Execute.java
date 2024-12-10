370d6783a9bfd18c4be8664172b92ea3f69107db
Felipe Pizarro Márquez <fpizarro92@live.cl>
2024-12-10 18:09:20 -0300

    Actualizacion produccion

--- src/main/java/cl/bilix/scrapper/service/Execute.java
+++ src/main/java/cl/bilix/scrapper/service/Execute.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
